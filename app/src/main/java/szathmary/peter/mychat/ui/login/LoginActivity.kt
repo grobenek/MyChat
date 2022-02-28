aa6cfc91f2cc4d56b66cd97ae1e8b5ae0c9611f2
Peter Szathmary <peter.szathmary01@gmail.com>
2022-02-28 22:04:17 +0100

    Basic UI for login

---
+++ app/src/main/java/szathmary/peter/mychat/ui/login/LoginActivity.kt
@@ -0,0 +1,36 @@
+package szathmary.peter.mychat.ui.login
+
+import android.app.Activity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.Toast
+import szathmary.peter.mychat.databinding.ActivityLoginBinding
+
+import szathmary.peter.mychat.R
+
+class LoginActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val username = binding.username
+        val password = binding.password
+        val login = binding.login
+        val loading = binding.loading
+
+
+    }
+}
